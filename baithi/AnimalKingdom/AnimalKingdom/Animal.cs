476a2eac688379e3c3edfcb660ad5886e3f72a01
nhoknam09877 <118426091+nhoknam09877@users.noreply.github.com>
2023-08-30 15:55:34 +0700

    Add files via upload

---
+++ baithi/AnimalKingdom/AnimalKingdom/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalKingdom
+{
+     class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public Animal(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}, Weight: {Weight}");
+        }
+
+        public void SetMe(double weight, string name)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+
+    class Lion : Animal
+    {
+        public Lion(string name, double weight) : base(name, weight)
+        {
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public Tiger(string name, double weight) : base(name, weight)
+        {
+        }
+    }
+}
---
+++ baithi/AnimalKingdom/AnimalKingdom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalKingdom
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Lion lion = new Lion("Lion ", 100);
+            Tiger tiger = new Tiger(" Tiger", 200);
+
+            Console.WriteLine("Lion Details:");
+            lion.Show();
+
+            Console.WriteLine("\nTiger Details:");
+            tiger.Show();
+
+          
+
+
+
+        }
+    }
+}
+
