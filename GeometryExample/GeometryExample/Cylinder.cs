501367efd3ed3a943aa98b8307351b5bfaea62d0
nhoknam09877 <namtqth2210018@fpt.edu.vn>
2023-08-30 15:27:47 +0700

    .

---
+++ GeometryExample/GeometryExample/Cylinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample
+{
+
+    class Cylinder
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+
+        public Cylinder(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        public void Process()
+        {
+            Console.WriteLine("Base Area: " + (Math.PI * Radius * Radius).ToString("F2"));
+            Console.WriteLine("Lateral Area: " + (2 * Math.PI * Radius * Height).ToString("F2"));
+            Console.WriteLine("Total Area: " + (2 * Math.PI * Radius * (Height + Radius)).ToString("F2"));
+            Console.WriteLine("Volume: " + (Math.PI * Radius * Radius * Height).ToString("F2"));
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("\nCylinder characteristics");
+            Console.WriteLine($"Radius: {Radius}, Height: {Height}");
+            Process();
+        }
+    }
+
+
+}
---
+++ GeometryExample/GeometryExample/program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Cylinder cylinder = new Cylinder(radius, height);
+
+            Console.WriteLine("\nCylinder Characteristics:");
+            cylinder.Process();
+
+           
+           
+        }
+    }
+}
